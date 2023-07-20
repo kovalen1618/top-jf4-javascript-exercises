20e3b07355aeadd6c9ab632d3845196fd363a951
kovalen1618 <kovalen1.618@gmail.com>
2023-07-20 15:26:31 -0500

    All tests passed, repeatString working

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let finalString = '';
 
+    if (num === 0) {
+        finalString = '';
+    } else if (num > 0) {
+        let i = 0;
+        while (i < num) {
+            finalString = finalString + string;
+            // string = string + string;
+            i++;
+        }
+    } else if (num < 0) {
+        finalString = 'ERROR';
+    }
+
+    return finalString;
 };
 
 // Do not edit below this line
