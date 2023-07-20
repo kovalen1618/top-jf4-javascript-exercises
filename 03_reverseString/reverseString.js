500fe76b01440dad61502f7fcc843518e28dd71f
kovalen1618 <kovalen1.618@gmail.com>
2023-07-20 15:51:35 -0500

    All tests passed, string reversed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // reverse() is an array method, so the string must be split first and then rejoined
+    let reversedString = string.split('').reverse().join("");
+    return reversedString;
 };
 
 // Do not edit below this line
