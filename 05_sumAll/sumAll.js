4f9fe2c75ba84ab56cb895a97cacaa712f60a081
kovalen1618 <kovalen1.618@gmail.com>
2023-07-21 12:56:04 -0500

    All tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    // Initialize how many integers there are
+    let numberOfIntegers = 0;
 
+    if (numOne !== Number(numOne) || numTwo !== Number(numTwo)) {
+        // Returns ERROR if argument is not a Number type
+        return 'ERROR';
+    } else {
+        // Math.abs turns negative values to positive
+        numberOfIntegers = Math.abs(numOne - numTwo) + 1;
+    }
+
+    // Sum of Arithmetic Sequence Formula
+    sum = (numberOfIntegers * (numOne + numTwo)) / 2;
+
+    // Returns ERROR if value is 0 or a negative value
+    return sum > 0 ? sum : 'ERROR';
 };
 
 // Do not edit below this line
