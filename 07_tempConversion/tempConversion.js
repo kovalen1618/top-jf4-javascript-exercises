d23de5abbfaa5e5dde9f1a165211bb4289060ec5
kovalen1618 <kovalen1.618@gmail.com>
2023-07-23 21:01:09 -0500

    Implemented Celsius-Fahrenheit and round function
    Created a round function to round the values of the
    final Celsius-Fahrenheit calculations to the nearest
    10th decimal place in a robust fashion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+// Custom function that allows for rounding to a specified decimal place
+const round = function(value, precision) {
+  // Initializes multiplier with 10 to the power of the precision argument, or 0 which will always equal 1
+  let multiplier = Math.pow(10, precision || 0);
+  // Does the roundabout calculation of finding the decimal place
+  return Math.round(value * multiplier) / multiplier;
+}
+
+const convertToCelsius = function(fahrenHeitTemp) {
+  return round(((fahrenHeitTemp - 32) * (5/9)), 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  return round(((celsiusTemp * (9/5)) + 32), 1);
 };
 
 // Do not edit below this line
