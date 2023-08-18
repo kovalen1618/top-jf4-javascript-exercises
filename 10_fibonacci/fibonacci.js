a69b053b1829eeeba782f42dfbdf57bae40ba6ce
kovalen1618 <kovalen1.618@gmail.com>
2023-08-18 11:24:26 -0500

    Implemented fibonacci sequence equation
    Learned about the fibonacci sequence and utilized
    an equation to find the exact number for the term used

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(term) {
+    if (term <= 0) {
+        return "OOPS";
+    } else {
+        return (
+            Math.round((Math.pow((1 + Math.sqrt(5)), term) - (Math.pow(1 - Math.sqrt(5)), term)) / (Math.pow(2, term) * Math.sqrt(5)))
+        );
+    }
 };
+
+console.log(fibonacci(-20))
 
 // Do not edit below this line
 module.exports = fibonacci;
